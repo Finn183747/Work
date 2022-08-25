a038d65122f79408c8ed55d83600e39fc52eaff9
Finn183747 <usmanovakbar47@gmail.com>
2022-08-25 18:01:36 +0500

    таблица умножений

--- src/Work03.java
+++ src/Work03.java
@@ -1,5 +1,6 @@
+
 import java.util.Scanner;
-public class Work02 {
+public class Work03 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println(" Number: ");
